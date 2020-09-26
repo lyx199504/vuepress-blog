046bcf89ffb1505bf5e2bacbd0e27a8dd2f2e0c9
lyx <765020742@qq.com>
2020-09-27 00:02:46 +0800

    新增首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,38 @@
 module.exports = {
+    title: '夜光的博客',
+    description: '',
+
+    head: [
+        ['link', { rel: 'icon', href: '/avatar.jpg' }],
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ],
+
     theme: 'reco',
     themeConfig: {
         author: '夜光',
+        logo: '/avatar.jpg',
+        authorAvatar: '/avatar.jpg',
 
         nav: [
             { text: '主页', link: '/', icon: 'reco-home' },
             { text: '时间线', link: '/timeline/', icon: 'reco-date' },
             { text: '关于我', link: '/about/', icon: 'reco-account' },
             { text: 'GitHub', link: 'https://github.com/lyx199504', icon: 'reco-github' }
+        ],
+        // 友链
+        friendLink: [
+            {
+                title: '午后南杂',
+                desc: 'Enjoy when you can, and endure when you must.',
+                email: '1156743527@qq.com',
+                link: 'https://www.recoluan.com'
+            },
+            {
+                title: 'vuepress-theme-reco',
+                desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+                avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+                link: 'https://vuepress-theme-reco.recoluan.com'
+            },
         ],
 
         type: 'blog',
@@ -22,5 +47,7 @@ module.exports = {
                 text: '标签'      // 默认文案 “标签”
             }
         }
-    }
+    },
+
+    startYear: '2020',
 }  
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+---
+home: true
+bgImage: '/bg.jpg'
+bgImageStyle: {
+  height: '500px'
+}
+heroText: null  # 取消首页标题
+tagline: null  # 取消首页描述
+---
