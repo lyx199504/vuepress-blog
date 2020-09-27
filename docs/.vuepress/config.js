e80cc2ab00da3e0079abfddce1b47382ff4f6f3c
lyx <765020742@qq.com>
2020-09-27 13:05:48 +0800

    新增配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,16 +9,26 @@ module.exports = {
 
     theme: 'reco',
     themeConfig: {
+        // 作者
         author: '夜光',
         logo: '/avatar.jpg',
         authorAvatar: '/avatar.jpg',
 
+        // 导航栏
         nav: [
             { text: '主页', link: '/', icon: 'reco-home' },
             { text: '时间线', link: '/timeline/', icon: 'reco-date' },
             { text: '关于我', link: '/about/', icon: 'reco-account' },
             { text: 'GitHub', link: 'https://github.com/lyx199504', icon: 'reco-github' }
         ],
+        // 自动形成侧边导航
+        sidebarDepth: 1,
+        displayAllHeaders: false,
+
+        // 搜索设置
+        search: true,
+        searchMaxSuggestions: 10,
+
         // 友链
         friendLink: [
             {
@@ -46,8 +56,15 @@ module.exports = {
                 location: 2,     // 在导航栏菜单中所占的位置，默认3
                 text: '标签'      // 默认文案 “标签”
             }
-        }
+        },
+
+        // 最后更新时间
+        lastUpdated: 'Last Updated',
+        // 项目开始时间
+        startYear: '2020',
     },
 
-    startYear: '2020',
+    markdown: {
+        lineNumbers: true
+    },
 }  
